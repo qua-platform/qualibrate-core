a0ba831c11ab321df125e702648bb3f40180b569
Serwan Asaad <serwan.asaad@quantum-machines.co>
2024-06-12 15:17:50 +0200

    add test cal

---
+++ calibrations/05_test_cal.py
@@ -0,0 +1,39 @@
+"""
+        TIME OF FLIGHT
+This sequence involves sending a readout pulse and capturing the raw ADC traces.
+The data undergoes post-processing to calibrate three distinct parameters:
+    - Time of Flight: This represents the internal processing time and the propagation delay of the readout pulse.
+    Its value can be adjusted in the configuration under "time_of_flight".
+    This value is utilized to offset the acquisition window relative to when the readout pulse is dispatched.
+
+    - Analog Inputs Offset: Due to minor impedance mismatches, the signals captured by the OPX might exhibit slight offsets.
+    These can be rectified in the configuration at: config/controllers/"con1"/analog_inputs, enhancing the demodulation process.
+
+    - Analog Inputs Gain: If a signal is constrained by digitization or if it saturates the ADC,
+    the variable gain of the OPX analog input can be modified to fit the signal within the ADC range of +/-0.5V.
+    This gain, ranging from -12 dB to 20 dB, can also be adjusted in the configuration at: config/controllers/"con1"/analog_inputs.
+"""
+
+from qualibrate import QualibrationNode, NodeParameters
+
+
+class Resonator:
+    pass
+
+
+class Parameters(NodeParameters):
+    resonator: str = "hi"
+    simulate: bool = True
+
+
+node = QualibrationNode(
+    name="test_cal",
+    parameters_class=Parameters,
+)
+
+node.parameters = Parameters()
+
+# Temporarily sleep 8 seconds
+from time import sleep
+
+sleep(8)
