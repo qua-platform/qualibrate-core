0230df9ae2a4dd7ba28bce6377bafeeb60f80a09
Serwan Asaad <serwan.asaad@quantum-machines.co>
2024-06-25 14:59:32 +0200

    update to test_cal

--- calibrations/05_test_cal.py
+++ calibrations/05_test_cal.py
@@ -1,4 +1,5 @@
 from qualibrate import QualibrationNode, NodeParameters
+from quam.components import *
 
 
 class Parameters(NodeParameters):
@@ -10,6 +11,13 @@ node = QualibrationNode(
     name="test_cal",
     parameters_class=Parameters,
 )
+# node.mode.interactive = True
+
+node.machine = BasicQuAM(
+    channels={
+        "ch1": SingleChannel(opx_output=("con1", 1), intermediate_frequency=100e6)
+    }
+)
 
 node.parameters = Parameters()
 
@@ -18,19 +26,13 @@ from time import sleep
 
 sleep(4)
 
-node.results = {"value1": 42}
-node.machine = {
-    "qubits": {
-        "q0": {
-            "id": 0,
-            "xy": {
-                "opx_output_I": "#/wiring/qubits/0/port_I",
-                "opx_output_Q": "#/wiring/qubits/0/port_Q",
-                "frequency_converter_up": "#/octave/RF_outputs/2",
-                "intermediate_frequency": -103658560.7967306,
-            },
-        }
-    }
+node.results = {
+    "resonator_val": node.parameters.resonator,
+    "simulate": node.parameters.simulate,
 }
+
+with node.record_state_updates():
+    channel = node.machine.channels["ch1"]
+    channel.intermediate_frequency = 50e6
 
 node.save()
