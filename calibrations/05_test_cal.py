2383c54fa442e088523ebc10df264a683a7bf897
Serwan Asaad <serwan.asaad@quantum-machines.co>
2024-06-28 13:52:12 +0200

    minor mods to test_cal

--- calibrations/05_test_cal.py
+++ calibrations/05_test_cal.py
@@ -1,19 +1,25 @@
 from qualibrate import QualibrationNode, NodeParameters
 from quam.components import *
+import numpy as np
+from time import sleep
+from matplotlib import pyplot as plt
 
 
 class Parameters(NodeParameters):
-    resonator: str = "hi"
-    simulate: bool = True
+    resonator: str = "q1.resonator"
+    sampling_points: int = 100
 
 
 node = QualibrationNode(
     name="test_cal",
     parameters_class=Parameters,
+    description="Test calibration that wait a few seconds, then plots random data.",
 )
+machine = node.machine
 # node.mode.interactive = True
 
-node.machine = BasicQuAM(
+
+machine = BasicQuAM(
     channels={
         "ch1": SingleChannel(opx_output=("con1", 1), intermediate_frequency=100e6)
     }
@@ -21,18 +27,15 @@ node.machine = BasicQuAM(
 
 node.parameters = Parameters()
 
-# Temporarily sleep 8 seconds
-from time import sleep
-
+# Sleep for a few seconds to simulate the calibration
 sleep(4)
 
-node.results = {
-    "resonator_val": node.parameters.resonator,
-    "simulate": node.parameters.simulate,
-}
+fig, ax = plt.subplots()
+ax.plot(np.random.rand(node.parameters.sampling_points))
+
+node.results = {"resonator_val": node.parameters.resonator, "results_fig": fig}
 
 with node.record_state_updates():
-    channel = node.machine.channels["ch1"]
-    channel.intermediate_frequency = 50e6
+    machine.channels["ch1"].intermediate_frequency = 50e6
 
 node.save()
