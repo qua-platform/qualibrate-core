783426539bfbc45c253cef654f62eef3e6f2e147
Serwan Asaad <serwan.asaad@quantum-machines.co>
2024-07-10 20:39:13 +0200

    slight modification to test_cal

--- calibrations/05_test_cal.py
+++ calibrations/05_test_cal.py
@@ -21,7 +21,12 @@ node = QualibrationNode(
 
 machine = node.machine = BasicQuAM(
     channels={
-        "ch1": SingleChannel(opx_output=("con1", 1), intermediate_frequency=100e6)
+        "ch1": SingleChannel(
+            opx_output=("con1", 1), intermediate_frequency=100e6
+        ),
+        "ch2": SingleChannel(
+            opx_output=("con1", 1), intermediate_frequency=100e6
+        ),
     }
 )
 
@@ -33,7 +38,9 @@ sleep(4)
 fig, ax = plt.subplots()
 xvals = np.linspace(-10, 10, node.parameters.sampling_points)
 gaussian = np.exp(-(xvals**2))
-noise = node.parameters.noise_factor * np.random.rand(node.parameters.sampling_points)
+noise = node.parameters.noise_factor * np.random.rand(
+    node.parameters.sampling_points
+)
 ax.plot(xvals, gaussian + noise)
 ax.set_xlabel("Frequency shift (Hz)")
 ax.set_ylabel("Signal amplitude (a.u.)")
@@ -42,5 +49,6 @@ node.results = {"resonator_val": node.parameters.resonator, "results_fig": fig}
 
 with node.record_state_updates():
     machine.channels["ch1"].intermediate_frequency = 50e6
+    machine.channels["ch2"].intermediate_frequency = 100e6
 
 node.save()
