b9413315b43e79683f182c9b91d019c0fc84f8fe
Serwan Asaad <serwan.asaad@quantum-machines.co>
2025-08-10 13:01:53 +0200

    Adding persistent namespace hack

--- qualibrate/utils/read_files.py
+++ qualibrate/utils/read_files.py
@@ -1,6 +1,9 @@
 from importlib.util import module_from_spec, spec_from_file_location
 from pathlib import Path
 from types import ModuleType
+from typing import Any, Dict
+
+_PERSIST_REGISTRY: Dict[str, dict[str, Any]] = {}
 
 
 def get_module_name(file_path: Path) -> str:
@@ -14,6 +17,9 @@ def import_from_path(module_name: str, file_path: Path) -> ModuleType:
     if spec is None:
         raise RuntimeError(f"Can't read spec from {file_path}")
     module = module_from_spec(spec)
+    # Provide a persistent namespace across runs of the same module_name
+    ns = _PERSIST_REGISTRY.setdefault(module_name, {})
+    module.__dict__["__persist__"] = ns
     if spec.loader is None:
         raise RuntimeError(f"Can't get loader from spec of file {file_path}")
     spec.loader.exec_module(module)
