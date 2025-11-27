7733803d9c190038ca4819288fd1a276cddbd0a2
EladZ-mac-laptop <elad.zaharan@quantum-machines.co>
2025-11-27 15:47:23 +0200

    enabling string comparisson

--- tests/unit/test_qualibration_graph/calibrations/nested_graph_for_tests_with_connect_on_failure.py
+++ tests/unit/test_qualibration_graph/calibrations/nested_graph_for_tests_with_connect_on_failure.py
@@ -35,7 +35,9 @@ with topg_ as topg:
     ) as subg:
         subg.add_node(library.nodes["test_cal"])
         subg.add_node(library.nodes["one_more_node"])
+        # subg.add_node(library.nodes["test_node"])
         subg.connect_on_failure(src="test_cal", dst="one_more_node")
+        # subg.connect(src="test_cal", dst="test_node")
 
 
     node1 = library.nodes["test_cal"]
@@ -44,4 +46,3 @@ with topg_ as topg:
     topg.add_node(node1)
 
     topg.connect(src=subg, dst=node1)
-
